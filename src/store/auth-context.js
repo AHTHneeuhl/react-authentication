bb8f3cb791325f6da742427ec01a13e8dd753adb
AHTHneeuhl <ahthneeuhl@gmail.com>
2021-12-14 20:57:23 +0530

    Finishing Touches

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,4 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
+
+let logoutTimer;
 
 const AuthContext = createContext({
   token: "",
@@ -16,25 +18,60 @@ const calculateRemainingTime = (expirationTime) => {
   return remainedTime;
 };
 
+const retrieveStoreToken = () => {
+  const storedToken = localStorage.getItem("token");
+  const storedExpirationDate = localStorage.getItem("expirationTime");
+
+  const remainingTime = calculateRemainingTime(storedExpirationDate);
+
+  if (remainingTime <= 60000) {
+    localStorage.removeItem("token");
+    localStorage.removeItem("expirationTime");
+    return null;
+  }
+
+  return {
+    token: storedToken,
+    duration: remainingTime,
+  };
+};
+
 export const AuthContextProvider = (props) => {
-  const initialToken = localStorage.getItem("token");
+  const tokenData = retrieveStoreToken();
+  let initialToken;
+
+  if (initialToken) {
+    initialToken = tokenData.token;
+  }
+
   const [token, setToken] = useState(initialToken);
 
   const userIsLoggedIn = !!token;
 
-  const logoutHandler = () => {
+  const logoutHandler = useCallback(() => {
     setToken(null);
     localStorage.removeItem("token");
-  };
+
+    if (logoutTimer) {
+      clearTimeout(logoutTimer);
+    }
+  }, []);
 
   const loginHandler = (token, expirationTime) => {
     setToken(token);
     localStorage.setItem("token", token);
+    localStorage.setItem("expirationTime", expirationTime);
 
     const remaindingTime = calculateRemainingTime(expirationTime);
 
-    setTimeout(logoutHandler, remaindingTime);
+    logoutTimer = setTimeout(logoutHandler, remaindingTime);
   };
+
+  useEffect(() => {
+    if (tokenData) {
+      logoutTimer = setTimeout(logoutHandler, tokenData.duration);
+    }
+  }, [tokenData, logoutHandler]);
 
   const contextValue = {
     token: token,
